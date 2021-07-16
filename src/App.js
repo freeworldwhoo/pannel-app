d126bc1a3c9bb080491907469732a7ac72a38261
pannel <rnbou@student.1337.ma>
2021-07-16 14:52:02 +0100

    connection and data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
 import Slider from './components/slider/slider.jsx'
 import Info from './components/info/info.jsx'
+import {useState,useEffect} from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [tougleGetId, setTougleGetId] = useState(false)
+  const [deviceId,setDeviceId] = useState("")
+  
+  useEffect(()=>{
+    const storage_id = localStorage.getItem('id')
+    const device_id =storage_id  ? storage_id  : ""
+    
+    const get_device_id = async()=>{
+      let data = new FormData()
+      data.append("auth_id",device_id)
+      await axios({
+        method:'post',
+        url:'http://192.168.137.1:8080/auth/',
+        data:data
+      }).then(res => {
+        try{
+          setDeviceId(res.data.auth_id)
+          localStorage.setItem('id',res.data.auth_id)
+
+        }
+        catch{
+          setDeviceId(device_id)
+        }
+      }).catch(()=>{
+        setDeviceId(device_id)})
+    }
+    get_device_id()
+   },[tougleGetId])
+
+   useEffect(()=>{
+    const update_device_info = async()=>{
+      if (deviceId.length > 6){
+
+        let data = new FormData()
+        data.append("name",'test')
+        data.append("latitude",'33.57310000')
+        data.append("longitude",'7.58900000')
+        await axios({
+          method:'put',
+          url:`http://192.168.137.1:8080/devices/${deviceId}/`,
+          data:data
+        }).then(res => {
+            console.log(res.data)
+        }).catch(()=>{
+          setTougleGetId(!tougleGetId)
+        })
+      }
+    }
+    update_device_info()
+
+   },[deviceId])
   return (
     <div className="App">
-      <Slider></Slider>
-      <Info></Info>
+      {/* <h1>{deviceId}</h1> */}
+      <Slider device_id = {deviceId} tougle_get_id = {setTougleGetId} get_id = {tougleGetId}></Slider>
+      <Info device_id = {deviceId} tougle_get_id = {setTougleGetId} get_id = {tougleGetId}></Info>
     </div>
   );
 }
--- src/components/info/info.jsx
+++ src/components/info/info.jsx
@@ -1,17 +1,22 @@
 import {useState,useEffect} from 'react'
 import {io} from 'socket.io-client'
 import './info.css'
+import axios from 'axios'
 
 const socket = io.connect('http://localhost:5000')
 
-function Info(){
+function Info({ tougle_get_id,get_id}){
     const d = new Date()
     const [t,setTime]=useState(d.toLocaleTimeString())
-    const [temp,setTemp] = useState(0+' °C')
-    const [pressure,setPressure] =  useState(0+' hPa')
-    const [hum,setHum] = useState(0 + ' %')
+    const [temp,setTemp] = useState(0)
+    const [pressure,setPressure] =  useState(0)
+    const [hum,setHum] = useState(0)
+    const [sendData,setSendData] = useState(false)
 
+    const device_id = localStorage.getItem('id')
+    
     const format = number => (Math.round(number * 100) / 100).toFixed(2);
+
     useEffect(()=>{
         setInterval(() => {
             
@@ -19,19 +24,47 @@ function Info(){
         }, 1000);
         socket.on('send_info',reading=>{
             const d = new Date()
-            console.log(d.toLocaleTimeString())
             setTime(d.toLocaleTimeString())
-            setTemp(`${format(reading.temperature)} °C`)
-            setPressure(`${format(reading.pressure)} hPa`)
-            setHum(`${format(reading.humidity)} %`)
+            setTemp(format(reading.temperature))
+            setPressure(format(reading.pressure) )
+            setHum(format(reading.humidity))
         })
+
+        
+        
+
     },[])
+
+    const device_send_data = async(send_data)=>{
+        const data = {"temperature":temp,
+                        "humidity":hum,
+                        "speed":40,
+                        "pressure":pressure}
+        console.log(data)
+        await axios({
+          method:'post',
+          url:`http://192.168.137.1:8080/devices/${device_id}/`,
+          data:data
+        }).then(res => {
+          console.log(res.data)
+        }).catch(()=>{
+            tougle_get_id(!get_id)
+          })
+      }
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            setSendData(!sendData)
+        },6000)
+        device_send_data(sendData)
+        return ()=> clearTimeout(timer)
+    },[sendData])
+
     return(
         <div className="infos-container">
             <div className="left-info">
-                <div className="info">{temp}<img src="/icons/t_icon.png" alt="temp" className="icon" width='96' height='96' /></div>
-                <div className="info">{pressure}<img src="/icons/b_icon.png" alt="b" className="icon" width='96' height='96' /></div>
-                <div className="info">{hum}<img src="/icons/h_icon.png" alt="h" className="icon" width='96' height='96' /></div>
+                <div className="info">{`${temp} °C`}<img src="/icons/t_icon.png" alt="temp" className="icon" width='96' height='96' /></div>
+                <div className="info">{`${pressure} hPa`}<img src="/icons/b_icon.png" alt="b" className="icon" width='96' height='96' /></div>
+                <div className="info">{`${hum} %`}<img src="/icons/h_icon.png" alt="h" className="icon" width='96' height='96' /></div>
             </div>
             <div className="right-info">
                 <div className="info"><img src="/icons/time_icon.png" alt="time" className="icon" width='96' height='96' />{t}</div>
